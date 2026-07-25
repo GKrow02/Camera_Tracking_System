0358d593f555c20faf0e57a95034176e694b9fd7
GKrow02 <102366007+GKrow02@users.noreply.github.com>
2026-07-25 12:56:05 -0400

    Update track_ObjectV1.py

--- track_ObjectV1.py
+++ track_ObjectV1.py
@@ -33,8 +33,8 @@ SMOOTHING_ALPHA = 0.2
 TARGET_HOLD_TIME = 1.0
 
 # Change either value to -1.0 if that servo moves away from the target.
-PAN_DIRECTION = -1.0
-TILT_DIRECTION = 1.0
+PAN_DIRECTION = 1.0
+TILT_DIRECTION = -1.0
 
 # This HSV range is yellow/green-yellow, suitable for an initial tennis-ball
 # or brightly colored target test. It is not a blue range.
